72d151a32fdaf6bb40fcb287774268671e0a8518
alex_motalex <tokyopac@gmail.com>
2024-09-16 22:11:48 +0300

    make build

---
+++ index.bef6dcac.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),l=Math.round((t.clientWidth-e.offsetWidth)/2)+"px",o=Math.round((t.clientHeight-e.offsetHeight)/2)+"px";e.style.left=l,e.style.top=o;
+//# sourceMappingURL=index.bef6dcac.js.map
